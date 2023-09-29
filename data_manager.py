dcc8e14aab15d88f7f3bb1a444fe6781d7210c01
nikita2060 <nikkyapandey@gmail.com>
2023-09-29 10:57:39 +0530

    :poop:Added function to update google sheet

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+SHEETY_ENDPOINT = "https://api.sheety.co/0d3b0683421d5adbc5a069b4803ee004/flightDeals/prices"
 
 class DataManager:
     def __init__(self):
@@ -8,14 +8,21 @@ class DataManager:
         self.get_sheet_data()
 
     def get_sheet_data(self):
-        sheet_url = "https://api.sheety.co/0d3b0683421d5adbc5a069b4803ee004/flightDeals/prices"
-        response = requests.get(url=sheet_url)
+
+        response = requests.get(url=SHEETY_ENDPOINT)
         flight_data_dict = json.loads(response.text)  # using json.loads() to convert json formatted string into  dictionary
 
         self.sheet_data = flight_data_dict["prices"]
-
     def update_sheet(self):
-        sheet_url = "https://api.sheety.co/0d3b0683421d5adbc5a069b4803ee004/flightDeals/prices/1"
-        response = requests.put(url=sheet_url)
-        print(response.text)
+        for item in self.sheet_data:
+            new_data = {
+                "price": {
+                    "iataCode": item["iataCode"]
+
+                        }
+            }
+
+            sheet_url = f"{SHEETY_ENDPOINT}/{item['id']}"
+            response = requests.put(url=sheet_url, json=new_data)
+            print(response.text)
 
