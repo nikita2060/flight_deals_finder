8969b191ebfcd28aefd968796f253b3f69ad972a
nikita2060 <nikkyapandey@gmail.com>
2023-10-01 21:05:51 +0530

    Adding notification manager related changes

--- flight_search.py
+++ flight_search.py
@@ -6,9 +6,8 @@ TEQUILA_API_KEY = os.getenv("TEQUILA_API_KEY")
 
 
 class FlightSearch:
-    def __init__(self):
-        self.cities = []
-
+    # def __init__(self):
+    #     self.cities = []
 
     # def respond(self):
     #     sheet_url = "https://api.sheety.co/0d3b0683421d5adbc5a069b4803ee004/flightDeals/prices"
@@ -16,9 +15,9 @@ class FlightSearch:
 
     def get_iata_code(city):
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-        header = {"api_key" :TEQUILA_API_KEY}
-        param = {"term":city, "location_types": "city"}
-        response = requests.get(url=location_endpoint,headers=header,params=param)
-        print(response.text())
-
-
+        header = {"api_key": TEQUILA_API_KEY}
+        param = {"term": city, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=header, params=param)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
--- main.py
+++ main.py
@@ -3,16 +3,21 @@
 
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
+notification_manager = NotificationManager()
 datamanager = DataManager()
 sheet_data = datamanager.sheet_data
+# print(sheet_data)
 
 def get_city_name():
     for row in sheet_data:
         if row['iataCode'] == '':
             city = row['city']
-            FlightSearch.get_iata_code(city)
+            code = FlightSearch.get_iata_code(city)
+            row['iataCode'] = code
 
 
 datamanager.update_sheet()
+notification_manager.send_sms("do it ashish")
 
