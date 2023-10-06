ddf0df1eeef5e7d659d2945449a670e9bc8db61d
nikita2060 <nikkyapandey@gmail.com>
2023-10-06 22:16:54 +0530

    Use of timedelta class from datetime module

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
-
+from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
 notification_manager = NotificationManager()
-datamanager = DataManager()
-sheet_data = datamanager.sheet_data
+data_manager = DataManager()
+sheet_data = data_manager.sheet_data
+flight_search = FlightSearch()
+
+
 # print(sheet_data)
 
 def get_city_name():
@@ -18,6 +21,9 @@ def get_city_name():
             row['iataCode'] = code
 
 
-datamanager.update_sheet()
-notification_manager.send_sms("do it ashish")
+data_manager.update_sheet()
 
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+# notification_manager.send_sms("do it")
